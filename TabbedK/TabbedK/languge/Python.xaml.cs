20815d76e2580126f68990e0ab6429becdabecd6
Opilane TTHK <opilane@tthk.ee>
2020-10-19 13:47:14 +0300

    Add project files.

---
+++ TabbedK/TabbedK/languge/Python.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedK.eöement
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Python : TabbedPage
+    {
+        public Python()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TabbedK/TabbedK/languge/c.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedK.eöement
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class c : TabbedPage
+    {
+        public c()
+        {
+            InitializeComponent();
+        }
+
+        private async void KButton_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Hi", "Continue", "Stop");
+        }
+    }
+}
